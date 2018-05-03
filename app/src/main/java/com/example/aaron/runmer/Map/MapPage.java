eb47f5770abf195847763f4274f3f0c076152140
tim2464 <tim2464@yahoo.com.tw>
2018-05-03 17:25:59 +0800

    Create Map UI

--- app/src/main/java/com/example/aaron/runmer/Map/MapPage.java
+++ app/src/main/java/com/example/aaron/runmer/Map/MapPage.java
@@ -1,12 +1,22 @@
 package com.example.aaron.runmer.Map;
 
-import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.example.aaron.runmer.Base.BaseActivity;
+import com.example.aaron.runmer.R;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
-public class MapPage  extends Fragment implements MapContract.View{
+public class MapPage  extends BaseActivity implements MapContract.View{
 
     private MapContract.Presenter mPresenter;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
 
     @Override
     public void setPresenter(MapContract.Presenter presenter) {

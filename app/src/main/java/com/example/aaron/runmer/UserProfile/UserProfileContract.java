a0cbe85f489dc98746a83864ac84120d0a2702f2
tim2464 <tim2464@yahoo.com.tw>
2018-05-24 20:28:46 +0800

    succeed to display avatar, name, age!

--- app/src/main/java/com/example/aaron/runmer/UserProfile/UserProfileContract.java
+++ app/src/main/java/com/example/aaron/runmer/UserProfile/UserProfileContract.java
@@ -3,13 +3,20 @@ package com.example.aaron.runmer.UserProfile;
 import com.example.aaron.runmer.Base.BasePresenter;
 import com.example.aaron.runmer.Base.BaseView;
 
+import java.util.HashMap;
+
 public interface UserProfileContract {
 
-    interface View extends BaseView<Presenter>{
+    interface View extends BaseView<Presenter> {
+        void showUserStatus(HashMap<String, String> mhashmap);
 
+        void showUserBirth(int age);
     }
 
-    interface Presenter extends BasePresenter{
+    interface Presenter extends BasePresenter {
 
+        void setUserStatus(HashMap<String, String> hashMapUserStatus);
+
+        void setUserAge(HashMap<String, String> hashMapUserStatus);
     }
 }
--- app/src/main/java/com/example/aaron/runmer/UserProfile/UserProfilePresenter.java
+++ app/src/main/java/com/example/aaron/runmer/UserProfile/UserProfilePresenter.java
@@ -1,6 +1,54 @@
 package com.example.aaron.runmer.UserProfile;
 
-public class UserProfilePresenter implements UserProfileContract.Presenter{
+import android.util.Log;
+
+import com.example.aaron.runmer.util.Constants;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class UserProfilePresenter implements UserProfileContract.Presenter {
+
+    private UserProfileContract.View mUserProfileView;
+
+    public UserProfilePresenter(UserProfileContract.View userProfileView) {
+        mUserProfileView = checkNotNull(userProfileView);
+        mUserProfileView.setPresenter(this);
+    }
+
+    @Override
+    public void setUserStatus(HashMap<String, String> hashMapUserStatus) {
+        mUserProfileView.showUserStatus(hashMapUserStatus);
+    }
+
+    @Override
+    public void setUserAge(HashMap<String, String> hashMapUserStatus) {
+        String userBirth = hashMapUserStatus.get(Constants.USER_FIREBASE_BIRTH);
+        Date date = null;
+
+        try {
+            date = new SimpleDateFormat("yyyy").parse(userBirth);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        Calendar calendar = Calendar.getInstance();//取得目前時間
+        int currentyear = calendar.get(Calendar.YEAR);
+//        Calendar c = Calendar.getInstance();
+        calendar.setTime(date);
+        int birthyear = calendar.get(Calendar.YEAR);
+        int age = currentyear - birthyear;
+        mUserProfileView.showUserBirth(age);
+        Log.d(Constants.TAG, "Age: " + age);
+        Log.d(Constants.TAG, "currentyear: " + currentyear);
+        Log.d(Constants.TAG, "birthyear: " + birthyear);
+    }
+
     @Override
     public void start() {
 

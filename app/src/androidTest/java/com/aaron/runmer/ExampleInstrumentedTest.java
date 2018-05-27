01338ded8f6bc431c3ae96c5cee24df526bea5b1
Aaron <tim2464@yahoo.com.tw>
2018-05-28 00:32:35 +0800

    move file & built apk & version code/name

--- app/src/androidTest/java/com/aaron/runmer/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/aaron/runmer/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.aaron.runmer;
+package com.aaron.runmer;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
--- app/src/test/java/com/aaron/runmer/ExampleUnitTest.java
+++ app/src/test/java/com/aaron/runmer/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.aaron.runmer;
+package com.aaron.runmer;
 
 import org.junit.Test;
 

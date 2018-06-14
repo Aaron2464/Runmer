f69c8115c9489e023d46750f9813699c6414e397
Aaron <tim2464@yahoo.com.tw>
2018-06-14 18:11:01 +0800

    try to write unit test

--- app/src/androidTest/java/com/aaron/runmer/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/aaron/runmer/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.aaron.runmer", appContext.getPackageName());
+        assertEquals("com.aaron.runmer", appContext.getPackageName());
     }
 }
